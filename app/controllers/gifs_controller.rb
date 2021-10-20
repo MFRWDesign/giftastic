06529f7f9f6dc38b97b11f391c064a2dfa28813b
Thomas Sample <thomas@mfrwdesign.com>
2021-10-20 11:56:42 -0400

    Deny all Gif privledges to logged out Users

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,5 +1,6 @@
 class GifsController < ApplicationController
   before_action :set_gif, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /gifs or /gifs.json
   def index
