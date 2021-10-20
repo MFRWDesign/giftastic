bad8d5983f24a9a78e7475e5aa595cb876b728a1
Thomas Sample <thomas@mfrwdesign.com>
2021-10-20 11:28:22 -0400

    Add Gifs for user uploads and run migration

---
+++ db/migrate/20211020151739_create_gifs.rb
@@ -0,0 +1,10 @@
+class CreateGifs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :gifs do |t|
+      t.string :title
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
