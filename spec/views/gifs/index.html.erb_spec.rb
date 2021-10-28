29adcd3fba200201f0e5c42991df3135ea95989d
Thomas Sample <thomas@mfrwdesign.com>
2021-10-28 10:53:07 -0400

    Fix specs accounting for pagination and validation
    Expect a redirect as a response to visiting the gifs/new
    when logged out
    Conditionally render pagination links on gifs#index and
    image tag on gifs#show

--- spec/views/gifs/index.html.erb_spec.rb
+++ spec/views/gifs/index.html.erb_spec.rb
@@ -3,16 +3,9 @@ require 'rails_helper'
 RSpec.describe "gifs/index", type: :view do
   before(:each) do
     @user = User.create!(email: 'bob@bob.com', password: 'bobbobob')
-    assign(:gifs, [
-      Gif.create!(
-        title: "Title",
-        user: @user
-      ),
-      Gif.create!(
-        title: "Title",
-        user: @user
-      )
-    ])
+    Gif.create!(title: "Title", user: @user)
+    Gif.create!(title: "Title", user: @user)
+    assign(:gifs, Gif.page(params[:page]).per(5))
   end
 
   it "renders a list of gifs" do
